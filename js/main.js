46af99c4816832e9e528308974b8846c563edde0
byehus <subbu.abbu@gmail.com>
2022-06-20 00:10:20 +0100

    added hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-let message;
+const btn = document.getElementById("menu-btn");
+const nav = document.getElementById("menu");
 
-message = "hello";
-
-console.log(message)
+btn.addEventListener("click", () => {
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
+});
